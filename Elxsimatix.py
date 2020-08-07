c7fd0dd73a391ba261fca000573a655d7b19865d
madhavikutty1988 <68739722+madhavikutty1988@users.noreply.github.com>
2020-08-07 14:33:05 +0530

    Modified the code to take the username and password from excel

--- Elxsimatix.py
+++ Elxsimatix.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.chrome.options import Options
-import Config
+import openpyxl
 
 chromeOptions=Options()
 chromeOptions.add_experimental_option("prefs",{"download.default_directory":"D:\Tools\Jenkins\Downloads"})
@@ -10,12 +10,16 @@ driver=webdriver.Chrome(executable_path="D:\Tools\Python\Driver\Chrome\chromedri
 driver.get("https://www.elxsimatix.net/")
 driver.maximize_window()
 driver.implicitly_wait(15)
-#Login to Elxsimatix
-username=driver.find_element_by_name("portlet_2_1{actionForm.username}")
-username.send_keys(Config.loginidStr)
-password=driver.find_element_by_name("portlet_2_1{actionForm.password}")
-password.send_keys(Config.passwordStr)
 
+#Login to Elxsimatix
+path="D:\Tools\Login.xlsx"
+workbook=openpyxl.load_workbook(path)
+sheet=workbook.active
+username=sheet.cell(3,1).value
+password=sheet.cell(3,2).value
+
+driver.find_element_by_name("portlet_2_1{actionForm.username}").send_keys(username)
+driver.find_element_by_name("portlet_2_1{actionForm.password}").send_keys(password)
 driver.find_element_by_xpath("//*[@id='loginForm']/table[1]/tbody/tr[6]/td[2]/input").click()
 
 #Parent window handle value
@@ -24,13 +28,13 @@ print(parent_window)
 parent_window_title=driver.title
 print(parent_window_title)
 #Open Skill Center Page
-driver.find_element_by_xpath("/html/body/div/div[2]/div[2]/span/div/table/tbody/tr/td[1]/div/table/tbody/tr[1]/td/div/div/div[2]/table/tbody/tr/td/ul/li[32]/a").click()
+driver.find_element_by_xpath("/html/body/div/div[2]/div[2]/span/div/table/tbody/tr/td[1]/div/table/tbody/tr[1]/td/div/div/div[2]/table/tbody/tr/td/ul/li[31]/a").click()
 child_window=driver.window_handles[1]
 print(child_window)
 
 #Switch to Skill Center Page
 driver.switch_to.window(child_window)
-time.sleep(5)
+time.sleep(15)
 #Child window handle value
 child_window_title=driver.title
 print(child_window_title)
@@ -40,6 +44,3 @@ driver.find_element_by_xpath("//*[@id='profile']/a").click()
 driver.find_element_by_xpath("/html/body/div[1]/div[1]/div/div/ul/li[3]/div/button").click()
 #Download Profile
 driver.find_element_by_xpath("/html/body/div[1]/div[1]/div/div/ul/li[3]/div/ul/li[2]/a[2]").click()
-
-
-
